d6304984a1eda3f0ff863edb5e0db0681ea639c8
Toheebay <successphu@gmail.com>
2025-07-20 22:38:30 -0700

    Fix: updated API base URL and resolved CSS conflict

--- finalproject/src/api.ts
+++ finalproject/src/api.ts
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:5000/api"; // change when deploying
+const API_BASE = "https://crptotask.onrender.com"; // change when deploying
 
 export const testAPI = async () => {
   const res = await fetch(`${API_BASE}/test`);
